4bdb5d33ab44707e003764f96b89ba938101f2be
Jeniamakarchik <eniamak@gmail.com>
2020-01-21 16:57:02 +0300

    add week3 lang test solution

--- test_languages/test_items.py
+++ test_languages/test_items.py
@@ -1,3 +1,4 @@
+import time
 from selenium.common.exceptions import NoSuchElementException
 
 
@@ -5,8 +6,10 @@ def test_contains_cart_button(browser):
     url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
     browser.get(url)
+    time.sleep(30)
     try:
         button = browser.find_element_by_class_name('btn-add-to-basket')
         assert True
     except NoSuchElementException:
+        browser.quit()
         assert False, "Button doesn't exist on the page."
